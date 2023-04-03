c17a37c41febb5b81919f77e071504052d54bc77
Lev Stambler <levstamb@gmail.com>
2023-04-03 11:42:22 +0100

    circuit wrapper

---
+++ circuit_wrapper/circuits/__tests__/compile_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd circuits/__tests__
+
+if [ -f ./powersOfTau28_hez_final_10.ptau ]; then
+    echo "powersOfTau28_hez_final_10.ptau already exists. Skipping."
+else
+    echo 'Getting powersOfTau28_hez_final_10.ptau'
+    wget https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_10.ptau
+fi
+
+
+# circom <Path> --r1cs --wasm --sym
