02af0cb71e8db9cf4d4cc7a5a00906fb54500730
Claudia Richoux <c@laudiacay.cool>
2023-04-04 12:38:21 +0200

    initial commit

---
+++ circuit_wrapper/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.spec.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/__tests__/**/*.ts',
+    '!<rootDir>/src/types/**/*.ts',
+    '!<rootDir>/src/cli.ts',
+  ],
+  globals: {
+    'ts-jest': {
+      diagnostics: false,
+    },
+  },
+};
