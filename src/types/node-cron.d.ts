cf3ef87537c936a7ecd5212291ebc8525b5ec63c
Claudia Richoux <c@laudiacay.cool>
2023-04-04 12:18:41 +0200

    got the skeleton, we need the proofs now.

---
+++ src/types/node-cron.d.ts
@@ -0,0 +1,14 @@
+declare module 'node-cron' {
+    export function schedule(
+      cronExpression: string,
+      callback: () => void,
+      options?: {
+        scheduled?: boolean;
+        timezone?: string;
+      }
+    ): {
+      start: () => void;
+      stop: () => void;
+    };
+  }
+  
