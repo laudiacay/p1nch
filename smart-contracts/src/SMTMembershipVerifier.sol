84d1d2d844430236119ba26a1b583e56b0469caa
Claudia Richoux <c@laudiacay.cool>
2023-04-09 00:30:39 +0200

    adding swapper logic

--- smart-contracts/src/SMTMembershipVerifier.sol
+++ smart-contracts/src/SMTMembershipVerifier.sol
@@ -1,10 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
 // a contract that verifies whether an update to the SMT is done right (and not including this ticket already!)
 contract SMTMembershipVerifier {
     // types
+    // TODO SHAME! SHAME!. there should be separate types for each proof. SHAME!!
     struct Proof {
-        uint[2] a;
-        uint[2][2] b;
-        uint[2] c;
+        uint256[2] a;
+        uint256[2][2] b;
+        uint256[2] c;
     }
 
     // public functions
@@ -13,13 +17,17 @@ contract SMTMembershipVerifier {
         return true;
     }
     // assert!(ticket_key \not \in root)
+
     function exclusionProof(Proof memory proof, uint256 root, uint256 ticket_key) public returns (bool r) {
         return true;
     }
     // assert!(root_after = root_before \union (ticket_key, 0))
     // assert!(ticket_key \not \in root_before)
-    function updateProof(Proof memory proof, uint256 root_before, uint256 root_after, uint256 ticket_key) public returns (bool r) {
+
+    function updateProof(Proof memory proof, uint256 root_before, uint256 root_after, uint256 ticket_key)
+        public
+        returns (bool r)
+    {
         return true;
     }
 }
-        
