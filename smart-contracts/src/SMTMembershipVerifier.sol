7c8913acf2cc4ea3aa6decb4cdd26d379ef15989
Lev Stambler <levstamb@gmail.com>
2023-04-10 19:09:29 +0100

    refactor to tok

--- smart-contracts/src/SMTMembershipVerifier.sol
+++ smart-contracts/src/SMTMembershipVerifier.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import {Verifier as SMTProcessorVerifier} from "@circuits/smt_processor_verify.sol";
-//import {Verifier as CommMembVerify} from "../../circuit_wrapper/build/comm_memb_verify.sol";
+import {Verifier as CommMembVerify} from "@circuits/comm_memb_verify.sol";
 
 // a contract that verifies whether an update to the SMT is done right (and not including this ticket already!)
 library SMTMembershipVerifier {
@@ -13,17 +13,6 @@ library SMTMembershipVerifier {
         uint256[2][2] b;
         uint256[2] c;
     }
-
-    // public functions
-    // assert!(ticket_key \in root)
-    // function inclusionProof(Proof calldata proof, uint256 root, uint256 ticket_hash) public returns (bool r) {
-    //     return true;
-    // }
-
-    // // assert!(ticket_key \not \in root)
-    // function exclusionProof(Proof calldata proof, uint256 root, uint256 ticket_hash) public returns (bool r) {
-    //     return true;
-    // }
 
     // assert!(root_after = root_before \union (ticket_hash, 0))
     // assert!(ticket_hash \not \in root_before)
@@ -42,7 +31,6 @@ library SMTMembershipVerifier {
         uint256[6] memory inputValues = [root_before, old_key_dummy, fn_0, fn_1, ticket_hash, root_after];
 
         return smt_proc_verifier.verifyProof(proof.a, proof.b, proof.c, inputValues);
-        return true;
     }
 
     // assert!(the commitment to this ticket is in the given root :))
@@ -50,9 +38,8 @@ library SMTMembershipVerifier {
         public
         returns (bool r)
     {
-        // CombMembVerify comm_memb_verify = CombMembVerify();
-        // uint256[2] memory inputValues = [ticket_hash_commitment, root];
-        // return comm_memb_verify.verifyProof(proof.a, proof.b, proof.c, inputValues);
-        return true;
+        CommMembVerify comm_memb_verify = new CommMembVerify();
+        uint256[2] memory inputValues = [ticket_hash_commitment, root];
+        return comm_memb_verify.verifyProof(proof.a, proof.b, proof.c, inputValues);
     }
 }
