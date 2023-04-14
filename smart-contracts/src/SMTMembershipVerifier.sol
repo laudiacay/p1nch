43997346832b55dea6e0d50d23968733a179ace0
Lev Stambler <levstamb@gmail.com>
2023-04-14 08:38:16 +0100

    compile with uniswap and warnings

--- smart-contracts/src/SMTMembershipVerifier.sol
+++ smart-contracts/src/SMTMembershipVerifier.sol
@@ -16,30 +16,46 @@ library SMTMembershipVerifier {
 
     // assert!(root_after = root_before \union (ticket_hash, 0))
     // assert!(ticket_hash \not \in root_before)
-    function updateProof(Proof calldata proof, uint256 root_before, uint256 root_after, uint256 ticket_hash)
-        public
-        returns (bool r)
-    {
+    function updateProof(
+        Proof calldata proof,
+        uint256 root_before,
+        uint256 root_after,
+        uint256 ticket_hash
+    ) public returns (bool r) {
         // TODO: should this be a
         SMTProcessorVerifier smt_proc_verifier = new SMTProcessorVerifier();
         // A dummy value for the proof as there is no "old key" in an insert
         uint256 old_key_dummy = 0;
-        // Specify that the proof should check that we are doing an **insert**
-        // I.e. the key did not exist before in the tree
-        uint256 fn_0 = 1;
-        uint256 fn_1 = 0;
-        uint256[6] memory inputValues = [root_before, old_key_dummy, fn_0, fn_1, ticket_hash, root_after];
+        uint256[4] memory inputValues = [
+            root_before,
+            old_key_dummy,
+            ticket_hash,
+            root_after
+        ];
 
-        return smt_proc_verifier.verifyProof(proof.a, proof.b, proof.c, inputValues);
+        return
+            smt_proc_verifier.verifyProof(
+                proof.a,
+                proof.b,
+                proof.c,
+                inputValues
+            );
     }
 
     // assert!(the commitment to this ticket is in the given root :))
-    function commitmentInclusion(Proof calldata proof, uint256 root, uint256 ticket_hash_commitment)
-        public
-        returns (bool r)
-    {
+    function commitmentInclusion(
+        Proof calldata proof,
+        uint256 root,
+        uint256 ticket_hash_commitment
+    ) public returns (bool r) {
         CommMembVerify comm_memb_verify = new CommMembVerify();
         uint256[2] memory inputValues = [ticket_hash_commitment, root];
-        return comm_memb_verify.verifyProof(proof.a, proof.b, proof.c, inputValues);
+        return
+            comm_memb_verify.verifyProof(
+                proof.a,
+                proof.b,
+                proof.c,
+                inputValues
+            );
     }
 }
---
+++ smart-contracts/src/SwapWrapper.sol
@@ -0,0 +1,54 @@
+// Inspred by: https://github.com/smye/1inch-swap/blob/master/contracts/SwapProxy.sol
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+
+contract SwapProxy {
+    ISwapRouter public immutable swapRouter;
+    // address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    // address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    // TODO: ma ze?
+    uint24 public constant feeTier = 3000;
+
+    struct SwapDescription {
+        address srcToken;
+        address dstToken;
+        address srcReceiver;
+        address dstReceiver;
+        uint256 amount;
+        uint256 minReturnAmount;
+        uint160 priceLimit;
+    }
+
+    // address immutable AGGREGATION_ROUTER_V3;
+
+    constructor(ISwapRouter _swapRouter) {
+        swapRouter = _swapRouter;
+    }
+
+    function swap(
+        SwapDescription memory swap
+    ) internal returns (uint256 amountOut) {
+        // Approve the router to spend srcToken.
+        TransferHelper.safeApprove(
+            swap.srcToken,
+            address(swapRouter),
+            swap.amount
+        );
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
+            .ExactInputSingleParams({
+                tokenIn: swap.srcToken,
+                tokenOut: swap.dstToken,
+                fee: feeTier,
+                recipient: msg.sender,
+                deadline: block.timestamp,
+                amountIn: swap.amount,
+                amountOutMinimum: swap.minReturnAmount,
+                sqrtPriceLimitX96: swap.priceLimit
+            });
+        amountOut = swapRouter.exactInputSingle(params);
+    }
+}
