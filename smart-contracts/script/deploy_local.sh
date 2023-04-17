f1be894f65f076066a4dc0022cfd6e31730a9d2b
Lev Stambler <levstamb@gmail.com>
2023-04-17 17:27:40 +0100

    deploys to local and addresses

--- smart-contracts/script/deploy_local.sh
+++ smart-contracts/script/deploy_local.sh
@@ -1,5 +1,5 @@
 LOCAL_RPC=http://127.0.0.1:8545
 source script/.env.local
 forge script script/DeployDummies.sol:DummyDeployScript --broadcast --verify --rpc-url $LOCAL_RPC --via-ir && \
-	forge script script/Deploy.sol:P1nchDeployScript --broadcast --verify --rpc-url $LOCAL_RPC --via-ir
-# forge script script/Deploy.sol:P1nchDeployScript --broadcast --verify --rpc-url $LOCAL_RPC --via-ir
+	forge script script/Deploy.sol:P1nchDeployScript --broadcast --verify --rpc-url $LOCAL_RPC --via-ir && \
+	ts-node script/get_addresses.ts localnet --resolveJsonModule=true
---
+++ smart-contracts/script/get_addresses.ts
@@ -0,0 +1,40 @@
+import { writeFileSync } from "fs";
+import deploy_data from "../broadcast/Deploy.sol/1/run-latest.json";
+import deploy_toks_data from "../broadcast/DeployDummies.sol/1/run-latest.json";
+import { join } from "path";
+
+const network = process.argv[2];
+const main = (network: string) => {
+  switch (network) {
+    case "mainnet":
+      throw new Error("Not implemented");
+    case "localnet":
+      //  TODO: This **overwrites** the existing addresses file.
+			// we need to have it not overwrite the existing file
+      const addrs_entries = deploy_data.transactions
+        .filter((tx: any) => tx.transactionType === "CREATE")
+        .map((tx: any) => [tx.contractName, tx.contractAddress]);
+      const addrs_toks_data = deploy_toks_data.transactions
+        .filter((tx: any) => tx.transactionType === "CREATE")
+        .map((tx: any) => [tx.contractName, tx.contractAddress]);
+      const deploy_addresses = { localnet: Object.fromEntries(addrs_entries) };
+      const tok_addresses = { localnet: Object.fromEntries(addrs_toks_data) };
+      writeFileSync(
+        join(__dirname, "../../pinch_ts/packages/assets/deploy_addresses.json"),
+        JSON.stringify(deploy_addresses)
+      );
+      writeFileSync(
+        join(
+          __dirname,
+          "../../pinch_ts/packages/assets/deploy_token_addresses.json"
+        ),
+        JSON.stringify(tok_addresses)
+      );
+      console.log("Done coppying over addresses");
+      return;
+    default:
+      throw new Error("Unknown network");
+  }
+};
+
+main(network);
